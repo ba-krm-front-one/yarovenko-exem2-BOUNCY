a70af3dcee8a4ba5c09db889bd0f95e2b9dc2083
Alexey <alexey.fbr@gmail.com>
2017-05-01 00:34:00 +0300

    added last block, second slider

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,18 @@
 /**
  * Created by Alexey on 30.04.2017.
  */
+function initMap() {
+    var uluru = {lat: 48.7332085, lng: 37.5937418};
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: uluru
+    });
+    var marker = new google.maps.Marker({
+        position: uluru,
+        map: map,
+        icon:'favicon.png',
+    });
+}
 
  ;(function ($) {
  "use strict";
@@ -10,7 +22,7 @@
         cellAlign: 'left',
         contain: true,
         arrows:false,
-        autoPlay: 6000
+        autoPlay: 1800
     });
 
         smoothScroll.init();
