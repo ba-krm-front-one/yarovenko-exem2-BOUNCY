8a37eed05d046186212acb97bca6ecbc5e820b77
Alexey <alexey.fbr@gmail.com>
2017-05-01 16:17:46 +0300

    added some change in code, make some refactoring, and other small changes

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,6 +1,10 @@
 /**
  * Created by Alexey on 30.04.2017.
  */
+
+/**
+ * function map.
+ */
 function initMap() {
     var uluru = {lat: 48.7332085, lng: 37.5937418};
     var map = new google.maps.Map(document.getElementById('map'), {
@@ -13,6 +17,10 @@ function initMap() {
         icon:'favicon.png',
     });
 }
+
+/**
+ * function slider.
+ */
 
  ;(function ($) {
  "use strict";
@@ -30,6 +38,34 @@ function initMap() {
         })
 
  })(jQuery);
+
+/**
+ * function scroll.
+ */
+
+;(function ($) {
+    "use strict";
+    $("#tabs li").click(function () {
+        $("#tabs li").removeClass("etkin").filter(this).addClass("etkin");
+        var filter_id = $(this).data("filter");
+        $("#items").isotope({
+            filter: filter_id,
+            animationOption: {
+                duration: 1000
+            }
+        });
+    });
+
+    $(document).ready(function () {
+        $('.etkin').click()
+    })
+
+})(jQuery);
+
+/**
+ * for table Isotope
+ */
+
 
 
 
